915d9724a9ae987b391ed988924e437ee94a0937
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-04-18 16:26:16 -0700

    Prototypal Inheritance, searching algorithm and hamsters full

---
+++ jsinfo/why_two_hamsters_are_full.js
@@ -0,0 +1,19 @@
+//http://javascript.info/task/hamster-proto
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  }
+};
+
+let speedy = {
+  __proto__: hamster,
+  stomach: []
+};
+
+let lazy = {
+  __proto__: hamster,
+  stomach: []
+};
